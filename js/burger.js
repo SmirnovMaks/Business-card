d1662046d331181950b9adf744c42d31fe71c144
Maksim Smirnov <perforator01@gmail.com>
2022-08-24 02:59:01 +0300

    адаптив

--- js/burger.js
+++ js/burger.js
@@ -1,11 +1,13 @@
 const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.header__nav');
 const body = document.querySelector('body');
+const language = document.querySelector('.home_language')
 console.log(burger);
 console.log(nav);
 
 burger.addEventListener('click', () => {
     nav.classList.toggle('active');
     burger.classList.toggle('active');
+    language.classList.toggle('active');
     body.classList.toggle('lock');
 })
